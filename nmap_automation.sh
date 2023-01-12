37ddf420085802da0f84af4e90914180a6eb9a76
Comitora <121478287+Comitora@users.noreply.github.com>
2023-01-12 13:15:55 +0530

    Update nmap_automation.sh

--- nmap_automation.sh
+++ nmap_automation.sh
@@ -5,8 +5,11 @@
 # Get input file containing list of URLs
 input_file=$1
 
-# Create a new file to store the cleaned URLs
-cleaned_file="cleaned_$input_file"
+# Get the path of the input file
+path=$(dirname "$input_file")
+
+# Create a new file to store the cleaned URLs in the same path as the input file
+cleaned_file="$path/cleaned_$(basename "$input_file")"
 
 # Remove "http://" and "https://" from the URLs
 sed 's/https:\/\///g;s/http:\/\///g' "$input_file" > "$cleaned_file"
@@ -17,12 +20,12 @@ while read -r url; do
     echo "Scanning $url"
     if nmap -sP "$url" | grep "Host is up" > /dev/null; then
         # If host is up, scan all open ports, version detection and verbose output
-        nmap -vv -sCV "$url" >> nmap_scan.txt
+        nmap -vv -sCV "$url" >> "$path/nmap_scan.txt"
     else
         # If host is down, use -Pn flag, version detection and verbose output
-        nmap -vv -sCV -Pn "$url" >> nmap_scan.txt
+        nmap -vv -sCV -Pn "$url" >> "$path/nmap_scan.txt"
     fi
 done < "$cleaned_file"
 
 echo "Cleaned URLs are stored in $cleaned_file"
-echo "Nmap
+echo "Nmap scan output is stored in $path/nmap_scan.txt"
